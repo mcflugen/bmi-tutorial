3553886e27ce8f28ab02e77996ad949cc2a70d7a
mcflugen <huttone@colorado.edu>
2015-04-24 16:39:30 -0600

    Added an install script for the tutorial engine.

---
+++ scripts/install-stack-on-beach.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+#
+# Install the WMT-EXE software stack.
+#
+# Install the entire software stack on beach.colorado.edu into the CURRENT
+# DIRECTORY. This is really only specific to beach in the location of java.
+#
+# Note:
+#   You must have ruby in your path. On beach, ruby is installed under
+#   /usr/local/ruby.
+
+PREFIX=$(pwd)
+JAVA=/usr/local/java/default/bin/java
+
+echo "Installing the entire wmt stack in:"
+echo ""
+echo "    $PREFIX"
+echo ""
+read -p "Are you sure you want to do this? " answer
+
+while true
+do
+    case $answer in
+      [y]* )
+          echo "OK."
+          break;;
+
+      * )
+          echo "Exiting"
+          exit
+          break;;
+    esac
+done
+
+echo "installing..."
+
+wget https://raw.githubusercontent.com/csdms/wmt-exe/master/scripts/install
+python ./install --with-java=$JAVA $PREFIX
